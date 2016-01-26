ce6fa9b43360b8f53473604781be3f9df457e243
Marcos Fernando Simon <marcosfsimon@gmail.com>
2016-01-25 22:27:59 -0200

    Classe para implementar o algoritmo de Dijkstra

---
+++ SearchAlgorithmComparison/algorithms/informedsearch/DijkstraAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using SearchAlgorithmComparison.algorithms.basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmComparison.algorithms.informedsearch
+{
+    public class DijkstraAlgorithm : BaseAlgorithm
+    {
+
+        public DijkstraAlgorithm()
+        {
+            //
+        }
+               
+        protected override void Execute(Node puzzle)
+        {            
+            throw new NotImplementedException();
+        }
+
+        public int GetCost()
+        {
+            //at a first try, I'll just count pieces out of the right place
+            //the less pieces it has, the better the path is;
+            //the method's name will change as soon as I implement more heuristics
+            throw new NotImplementedException("Não implementado!");
+        }
+    }
+}
